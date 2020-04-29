eb14831bf8aab9f6d0072dfa7d9dc896840dea10
matias <matiasallegranza06@yahoo.com.ar>
2020-04-29 16:10:17 -0300

    Probando de conectar a una BBDD de access con pass

--- amema/src/data/Conector.java
+++ amema/src/data/Conector.java
@@ -2,14 +2,22 @@ package data;
 
 import java.sql.*;
 import util.ApplicationException;
+import util.CryptCodecOpener;
 
 
 public class Conector {
 	
-	private static String server = "jdbc:mysql://localhost/amemaweb?serverTimezone=UTC"; //Nombre del servidor
+	/*private static String server = "jdbc:mysql://localhost/amemaweb?serverTimezone=UTC"; //Nombre del servidor
 	private static String user = "root"; //Usuario de BBDD en el servidor
 	private static String pass = "root"; //Contraseña de BBDD en el servidor
-	private static String driver = "com.mysql.cj.jdbc.Driver"; //Driver de conexion JDBC
+	private static String driver = "com.mysql.cj.jdbc.Driver"; //Driver de conexion JDBC*/
+	
+	
+	private static String driver = "net.ucanaccess.jdbc.UcanaccessDriver";
+	private static String server = "jdbc:ucanaccess://C:/AMEMA/AMEMA.mdb;jackessOpener=CryptCodecOpener";
+	private static String user = "";
+	private static String pass = "Meriadoc";
+	
 	private Connection conexion;
 	private int cantConn = 0; //cantidad de conexiones a la BBDD
 	
@@ -17,7 +25,7 @@ public class Conector {
 	public Conector(){
 		try{
 			Class.forName(driver);
-			conexion = DriverManager.getConnection(server, user, pass);
+			conexion = DriverManager.getConnection(server,"sa","meriadoc");
 		}
 		catch(ClassNotFoundException | SQLException e){
 			System.out.println("--------");
--- amema/src/pruebas/Prueba.java
+++ amema/src/pruebas/Prueba.java
@@ -2,38 +2,29 @@ package pruebas;
 
 import java.util.ArrayList;
 
-import controladores.CtrlFamilia;
-import entidades.Familia;
+
+import util.ApplicationException;
+import data.Conector;
 
 public class Prueba {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		CtrlFamilia cf = new CtrlFamilia();
-		ArrayList<Familia> lista = new ArrayList<>();
+		Conector conn = new Conector();
 		
-		/*Metodos a probar*/
+		conn.abrirConn();
+		System.out.println("Se abrio la conexion!!");
 		
-		Familia flia = new Familia();
-		
-		flia=cf.consultaFamilia(3);
-		
-		System.out.println(flia.getCodigo());
-		System.out.println(flia.getDescripcion());
-		System.out.println(flia.getBonificacion());
-		
-		/*Listado de familias*/
-		lista = cf.listaFamilias("codigo");
-		System.out.println("Familias");
-		System.out.println("----");
-		System.out.println();
-		for (Familia fl : lista) {
-			System.out.println("Codigo: "+fl.getCodigo());
-			System.out.println("Descripcion: "+fl.getDescripcion());
-			System.out.println("Bonificacion: "+fl.getBonificacion() );
-			System.out.println();
+		try {
+			conn.cerrarConn();
+		} catch (ApplicationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+		System.out.println("Se cerro la conexión!!");
+
+		
 	}
 
 }
